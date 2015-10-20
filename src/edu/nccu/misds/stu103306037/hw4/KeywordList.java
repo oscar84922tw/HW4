63a270893ae133ea688cdbd70d56a3274d9c9f94
oscar84922tw <oscar84922tw@gmail.com>
2015-10-20 20:11:16 +0800

    final

--- src/edu/nccu/misds/stu103306037/hw4/KeywordList.java
+++ src/edu/nccu/misds/stu103306037/hw4/KeywordList.java
@@ -13,7 +13,7 @@ public class KeywordList {
 	public void add(Keyword keyword) {
 		for (int i = 0; i < lst.size(); i++) {
 			Keyword k = lst.get(i);
-			if(k.count>=keyword.count){
+			if (k.count >= keyword.count) {
 				lst.add(i, keyword);
 				System.out.println("Done");
 				return;
@@ -22,4 +22,170 @@ public class KeywordList {
 		lst.add(keyword);
 		System.out.println("Done");
 	}
+
+	public void outputIndex(int c) {
+		if (c < lst.size()) {
+			Keyword k = lst.get(c);
+			System.out.println(k);
+		} else {
+			System.out.println("Invalid Operation");
+		}
+
+	}
+
+	public void outputCount(int c) {
+		int tmp = 0;
+		for (int i = 0; i < lst.size(); i++) {
+			Keyword k = lst.get(i);
+			if (k.count == c) {
+				System.out.println(k);
+				tmp++;
+			}
+			if (tmp == 0) {
+				System.out.println("NotFount");
+			}
+		}
+	}
+
+	public void outputHas(String str) {
+		int tmp = 0;
+		int fromIdx = 0;
+		for (int i = 0; i < lst.size(); i++) {
+			Keyword k = lst.get(i);
+			while ((k.name.indexOf(str, fromIdx)) != -1) {
+				System.out.println(k.name);
+				tmp++;
+				break;
+			}
+
+		}
+		if (tmp == 0) {
+			System.out.println("NotFount");
+		}
+	}
+
+	public void outputName(String name) {
+		int tmp = 0;
+		for (int i = 0; i < lst.size(); i++) {
+			Keyword k = lst.get(i);
+			if (k.name.equals(name)) {
+				System.out.println(k);
+				tmp++;
+			}
+		}
+		if (tmp == 0) {
+			System.out.println("Not Found");
+		}
+
+	}
+
+	public void outputFirstN(int n) {
+		int tmp = 0;
+		for (int i = 0; i < lst.size(); i++) {
+			Keyword k = lst.get(i);
+			if (i < n) {
+				System.out.println(k);
+				tmp++;
+			}
+		}
+		if (tmp == 0) {
+			System.out.println("Invalid Operation");
+		}
+	}
+
+	public void outputScore() {
+		double sum = 0;
+		for (int i = 0; i < lst.size(); i++) {
+			Keyword k = lst.get(i);
+			sum += (k.count) * (k.weight);
+		}
+		System.out.println(sum);
+	}
+
+	public void deleteIndex(int c) {
+		if (c < lst.size()) {
+			lst.remove(c);
+			System.out.println("Done");
+		} else {
+			System.out.println("Invalid Operation");
+		}
+	}
+
+	public void deleteCount(int c) {
+		int tmp = 0;
+		for (int i = 0; i < lst.size(); i++) {
+			Keyword k = lst.get(i);
+			if (k.count == c) {
+				lst.remove(i);
+				tmp++;
+			}
+		}
+		if (tmp == 0) {
+			System.out.println("Not Found");
+		} else {
+			System.out.println("Done");
+		}
+	}
+
+	public void deleteHas(String str) {
+		int tmp = 0;
+		int fromIdx = 0;
+		for (int i = 0; i < lst.size(); i++) {
+			Keyword k = lst.get(i);
+			while ((k.name.indexOf(str, fromIdx)) != -1) {
+				lst.remove(i);
+				tmp++;
+				break;
+			}
+		}
+		if (tmp == 0) {
+			System.out.println("Not Found");
+		} else {
+			System.out.println("Done");
+		}
+	}
+
+	public void deleteName(String name) {
+		int tmp = 0;
+		for (int i = 0; i < lst.size(); i++) {
+			Keyword k = lst.get(i);
+			if (k.name.equals(name)) {
+				lst.remove(i);
+				tmp++;
+			}
+		}
+		if (tmp == 0) {
+			System.out.println("Not Found");
+		} else {
+			System.out.println("Done");
+		}
+	}
+
+	public void deleteFirstN(int n) {
+		// int tmp = 0;
+
+		if (n > lst.size()) {
+			System.out.println("Invalid Operation");
+
+			// tmp++;
+
+		} else {
+			for (int i = 0; i < lst.size(); i++) {
+				// Keyword k = lst.get(i);
+				if (i < n) {
+					// System.out.println(k);
+					lst.remove(i);
+
+					// }
+				}
+				System.out.println("Done");
+			}
+		}
+	}
+	
+	public void deleteAll(){
+		lst.removeAll(lst);
+	}
+
+
 }
--- src/edu/nccu/misds/stu103306037/hw4/Main.java
+++ src/edu/nccu/misds/stu103306037/hw4/Main.java
@@ -5,18 +5,91 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while(sc.hasNextLine()){
+		KeywordList kLsit = new KeywordList();
+		while (sc.hasNextLine()) {
 			String cmd = sc.next();
 			switch (cmd) {
-			case "add":
-				
+			case "add": {
+				String name = sc.next();
+				int count = sc.nextInt();
+				float weight = sc.nextFloat();
+				Keyword k = new Keyword(name, count, weight);
+				kLsit.add(k);
+			}
 				break;
-			case "":
+			case "outputIndex": {
+				int x = sc.nextInt();
+				kLsit.outputIndex(x);
+			}
+				break;
+			case "outputCount": {
+				int count = sc.nextInt();
+				kLsit.outputCount(count);
+			}
+				break;
+			case "outputHas":
+			{
+				String arg = sc.next();
+				kLsit.outputHas(arg);
+			}
+				break;
+			case "outputName":
+			{
+				String name = sc.next();
+				kLsit.outputName(name);
+			}
+				break;
+			case "outputFirstN":
+			{
+				int N = sc.nextInt();
+				kLsit.outputFirstN(N);
+			}
+				break;
+			case "outputScore":
+			{
+				kLsit.outputScore();
+			}
+				break;
+			case "deleteIndex":
+			{
+				int x = sc.nextInt();
+				kLsit.deleteIndex(x);
+			}
+				break;
+			case "deleteCount":
+			{
+				int x = sc.nextInt();
+				kLsit.deleteCount(x);
+			}
+				break;
+			case "deleteHas":
+			{
+				String arg = sc.next();
+				kLsit.deleteHas(arg);
+			}
+				break;
+			case "deleteName":
+			{
+				String name = sc.next();
+				kLsit.deleteName(name);
+			}
+				break;
+			case "deleteFirstN":
+			{
+				int N = sc.nextInt();
+				kLsit.deleteFirstN(N);
+			}
+				break;
+
+			case "deleteAll":
+			{
+				kLsit.deleteAll();
+			}
 			default:
 				break;
 			}
 		}
-	sc.close();
+		sc.close();
 	}
-	
+
 }
